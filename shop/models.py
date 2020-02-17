4801fb065230bc700fbefe119b60364104032fc2
Brijesh kalkani <brijeshkalkani4@gmail.com>
2020-02-17 10:38:36 +0530

    second commit

--- shop/models.py
+++ shop/models.py
@@ -6,11 +6,11 @@ class Product(models.Model):
     """docstring for Product."""
 
     product_id = models.AutoField
-    product_name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=50,default='')
     category = models.CharField(max_length=50,default='')
     subcategory = models.CharField(max_length=50,default='')
     price = models.IntegerField(default=0)
-    desc = models.CharField(max_length=300)
+    desc = models.CharField(max_length=300,default='')
     pub_date = models.DateField()
     image = models.ImageField(upload_to="shop/images",default='')
 
